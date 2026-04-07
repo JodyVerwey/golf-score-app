01e3f0dcac3557a7cb63aa45478d412b17d71d74
Jody Verwey <st10082744@vcconnect.edu.za>
2026-04-07 16:50:47 +0200

    2026.04.07 Added capcatiro and tested on emulator

---
+++ android/app/src/main/java/com/gbggolf/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gbggolf.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gbggold.app',
+  appName: 'Golf Score',
+  webDir: 'dist/golf-score-app/browser',
+};
+
+export default config;
